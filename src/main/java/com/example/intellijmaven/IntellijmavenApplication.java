9b566a31f41267364b4ec894685ffdb4a40b5988
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-11-01 06:19:08 -0400

    added project

---
+++ src/main/java/com/example/intellijmaven/IntellijmavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.intellijmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intellijmaven/IntellijmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intellijmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
